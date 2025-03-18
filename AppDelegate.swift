d4ef54f52a6703694a01ef9df3faa14ee1986539
David Ben Gurion <87116209+david-ben-gurion@users.noreply.github.com>
2025-03-18 12:53:03 +1100

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -101,45 +101,113 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
 /*
 import UIKit
+import BackgroundTasks
+import HealthKit
 
-class AppDelegate: UIResponder, UIApplicationDelegate, URLSessionDelegate, URLSessionTaskDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, URLSessionDelegate {
     var window: UIWindow?
-    var uploadTimer: Timer?
     var backgroundSession: URLSession?
-    
-    @objc func uploadDataToS3() {
-        HealthStoreManager.shared.fetchAndUploadHealthData { success in
+    let backgroundTaskIdentifier = "com.yourApp.backgroundUpload"
+    let healthStore = HKHealthStore()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: backgroundTaskIdentifier, using: nil) { task in
+            self.handleHealthDataUpload(task: task as! BGAppRefreshTask)
+        }
+        
+        requestHealthKitAuthorization()
+        scheduleDailyUpload()
+
+        return true
+    }
+
+    func requestHealthKitAuthorization() {
+        let dataTypesToRead: Set<HKSampleType> = [
+            HKObjectType.quantityType(forIdentifier: .stepCount)!,
+            HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!,
+            HKObjectType.quantityType(forIdentifier: .heartRate)!,
+            HKObjectType.quantityType(forIdentifier: .restingHeartRate)!,
+            HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
+            HKObjectType.quantityType(forIdentifier: .basalEnergyBurned)!,
+            HKObjectType.quantityType(forIdentifier: .appleStandTime)!,
+            HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+            HKObjectType.quantityType(forIdentifier: .appleExerciseTime)!,
+            HKObjectType.quantityType(forIdentifier: .flightsClimbed)!,
+            HKObjectType.quantityType(forIdentifier: .height)!,
+            HKObjectType.quantityType(forIdentifier: .bodyMass)!
+        ]
+
+        healthStore.requestAuthorization(toShare: nil, read: dataTypesToRead) { success, error in
             if success {
-                print("Data uploaded successfully!")
-            } else {
-                print("Failed to upload data.")
+                self.enableBackgroundDeliveryForHealthData()
+            } else if let error = error {
+                print("HealthKit authorization failed: \(error)")
             }
         }
     }
-    
+
+    func enableBackgroundDeliveryForHealthData() {
+        let dataTypes = [
+            HKObjectType.quantityType(forIdentifier: .stepCount)!,
+            HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!,
+            HKObjectType.quantityType(forIdentifier: .heartRate)!,
+            HKObjectType.quantityType(forIdentifier: .restingHeartRate)!,
+            HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
+            HKObjectType.quantityType(forIdentifier: .basalEnergyBurned)!,
+            HKObjectType.quantityType(forIdentifier: .appleStandTime)!,
+            HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+            HKObjectType.quantityType(forIdentifier: .appleExerciseTime)!,
+            HKObjectType.quantityType(forIdentifier: .flightsClimbed)!,
+            HKObjectType.quantityType(forIdentifier: .height)!,
+            HKObjectType.quantityType(forIdentifier: .bodyMass)!
+        ]
+
+        for dataType in dataTypes {
+            healthStore.enableBackgroundDelivery(for: dataType, frequency: .immediate) { success, error in
+                if success {
+                    print("Enabled background delivery for \(dataType.identifier)")
+                } else if let error = error {
+                    print("Failed to enable background delivery for \(dataType.identifier): \(error)")
+                }
+            }
+        }
+    }
+
     func scheduleDailyUpload() {
-        let calendar = Calendar.current
-        let currentTime = Date()
-        var nextUploadTime = calendar.date(bySettingHour: 17, minute: 0, second: 0, of: currentTime)
-        if let nextTime = nextUploadTime, nextTime < currentTime {
-            nextUploadTime = calendar.date(byAdding: .day, value: 1, to: nextTime)
-        }
-        if let nextUploadTime = nextUploadTime {
-            let timeInterval = nextUploadTime.timeIntervalSince(currentTime)
-            uploadTimer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(uploadDataToS3), userInfo: nil, repeats: true)
+        let request = BGAppRefreshTaskRequest(identifier: backgroundTaskIdentifier)
+        request.earliestBeginDate = Date(timeIntervalSinceNow: 24 * 60 * 60)
+        
+        do {
+            try BGTaskScheduler.shared.submit(request)
+            print("Daily background task scheduled.")
+        } catch {
+            print("Could not schedule background task: \(error.localizedDescription)")
         }
     }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        scheduleDailyUpload()
+
+    func handleHealthDataUpload(task: BGAppRefreshTask) {
+        HealthStoreManager.shared.fetchAndUploadHealthData { success in
+            if success {
+                print("Data uploaded successfully!")
+                task.setTaskCompleted(success: true)
+                self.scheduleDailyUpload()
+            } else {
+                print("Data upload failed.")
+                task.setTaskCompleted(success: false)
+            }
+        }
+        
+        task.expirationHandler = {
+            print("Background task expired.")
+            task.setTaskCompleted(success: false)
+        }
     }
-    
+
     func applicationDidEnterBackground(_ application: UIApplication) {
         let config = URLSessionConfiguration.background(withIdentifier: "com.yourApp.backgroundUpload")
         backgroundSession = URLSession(configuration: config, delegate: self, delegateQueue: nil)
-        uploadDataToS3()
     }
-    
+
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
         if let error = error {
             print("Background upload failed with error: \(error.localizedDescription)")
@@ -147,14 +215,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, URLSessionDelegate, URLSe
             print("Background upload completed successfully.")
         }
     }
-    
-    private func urlSession(_ session: URLSession, uploadTask: URLSessionUploadTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) {
-        print("Upload progress: \(totalBytesSent) / \(totalBytesExpectedToSend) bytes sent.")
-    }
-    
+
     func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
         backgroundSession?.getTasksWithCompletionHandler { (_, _, uploadTasks) in
-            if uploadTasks.count == 0 {
+            if uploadTasks.isEmpty {
                 completionHandler()
             }
         }
